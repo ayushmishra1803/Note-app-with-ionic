3e360d90c16414745a6f216c61666975e0e3863d
Ayush-Mishra <ayushmishra0810@gmail.com>
2020-07-03 03:37:52 +0530

    Updates

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"],
 })
 export class HomePage {
-
   constructor() {}
-
+  note: string;
+  notes: string[] = [];
+  onenter() {
+    this.notes.push(this.note);
+  }
+  ondelete(no) {
+    this.notes.splice(this.notes.indexOf(no,1))
+  }
 }
